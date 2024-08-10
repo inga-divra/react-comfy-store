f338c003bc92a43fc838b33d6291960f574d9e16
inga-divra <ingady@gmail.com>
2024-08-10 19:40:04 +0300

    Enhanced the SubmitBtn component to handle form submission states using useNavigation from react-router-dom
    The button now displays a loading spinner and disables itself during submission, improving user feedback and preventing duplicate submissions.

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,10 +1,17 @@
+import { useNavigation } from "react-router-dom"
 
 const SubmitBtn = ({ text }) => {
+    const navigation = useNavigation()
+    const isSubmitting = navigation.state === "submitting"
     return (
         <button
-            className="btn btn-primary uppercase"
-            type="button">
-            {text}
+            className="btn btn-primary uppercase btn-block"
+            type="button"
+            disabled={isSubmitting}>
+            {isSubmitting ? <>
+                <span className="loading loading-spinner"></span>
+                sending...
+            </> : text || "submit"}
         </button>
     )
 }
