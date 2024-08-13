57a35d4eb4e8052f44ec048d982d83c718cb785a
inga-divra <ingady@gmail.com>
2024-08-13 21:01:30 +0300

    Set up product grid with dynamic data loading, Euro price formatting, and responsive layout

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,11 @@ const productionUrl = 'https://strapi-store-server.onrender.com/api';
 export const customFetch = axios.create({
   baseURL: productionUrl,
 });
+
+export const formatPrice = (price) => {
+  const euroAmount = new Intl.NumberFormat('de-DE', {
+    style: 'currency',
+    currency: 'EUR',
+  }).format((price / 100).toFixed(2));
+  return euroAmount;
+};
