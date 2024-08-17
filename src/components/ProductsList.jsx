7be1dad2b3f149632ba8d63adcd78c70dd44146e
inga-divra <ingady@gmail.com>
2024-08-17 16:33:21 +0300

    Setup `ProductsList` component
     - Imported `Link` and `useLoaderData` from `react-router-dom` for routing and data handling.
     - Imported `formatPrice` utility to handle currency formatting.
     - Rendered product cards by mapping over `products` data, displaying product image, title, company, and formatted price.
     - Applied responsive styling with hover effects for product cards and images.

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -6,29 +6,35 @@ const ProductsList = () => {
     const { products } = useLoaderData()
 
     return (
-        <div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+        <div className='mt-12 grid gap-y-8 '>
             {products.map((product) => {
-                const { title, price, image } = product.attributes
+                const { title, price, image, company } = product.attributes
                 const euroAmount = formatPrice(price)
-                return <Link
-                    className='cart w-full shadow-xl hover:shadow-2xl transition duration-300'
-                    key={product.id}
-                    to={`/products/${product.id}`}>
-                    <figure className='px-4 pt-4'>
-                        <img
-                            className='rounded-xl h-64 md:h-48 w-full object-cover'
+                return (
+                    <Link
+                        key={product.id}
+                        to={`/products/${product.id}`}
+                        className='p-8 rounded-lg flex flex-col sm:flex-row gap-y-4 
+                        flex-wrap sm:flex-row gap-y-4 flex-wrap bg-base-100 shadow-xl 
+                        hover:shadow-2xl duration-300 group'>
+                        <img className='h-24 w-24 rounded-lg sm:h-32 sm:w-32 
+                        object-cover group-hover:scale-105 transition duration-300'
                             src={image}
                             alt={title} />
-                    </figure>
-                    <div className='card-body items-center text-center'>
-                        <h2 className='card-title capitalize tracking-wider'>
-                            {title}
-                        </h2>
-                        <span className='text-secondary'>
+                        <div className='ml-0 sm:ml-16'>
+                            <h3 className='capitalize font-medium text-lg'>
+                                {title}
+                            </h3>
+                            <h4 className='capitalize text-md text-neutral-content'>
+                                {company}
+                            </h4>
+                        </div>
+                        <p className='font-medium ml-0 sm:ml-auto text-lg'>
                             {euroAmount}
-                        </span>
-                    </div>
-                </Link>
+                        </p>
+                    </Link>
+                )
+
             })}
         </div>
     )
