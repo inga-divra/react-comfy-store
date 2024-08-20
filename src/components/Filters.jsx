2604c0d499860d21de2bba1a012834867ca374f0
inga-divra <ingady@gmail.com>
2024-08-20 12:01:27 +0300

    Setup search input in `Filters` component
      - Added a search input field using the `FormInput` component.
      - Configured the input field to be of type 'search' with a label 'search product'.
      - Assigned a name attribute of 'search' for form data handling.
      - Applied the 'input-sm' class for a smaller input size.
      - Positioned the input within a responsive grid layout to ensure proper alignment with other form elements.

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,8 +1,29 @@
+import { Form, useLoaderData, Link } from "react-router-dom"
+import FormInput from "./FormInput"
 
+Form
 const Filters = () => {
-    return (
-        <h1>Filters</h1>
-    )
+    return <Form
+        className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 
+    sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+        {/* SEARCH */}
+        <FormInput
+            type='search'
+            label='search product'
+            name='search'
+            size='input-sm' />
+        {/* BUTTONS */}
+        <button
+            className='btn btn-primary btn-sm uppercase'
+            type='submit'>
+            search
+        </button>
+        <Link
+            className='btn btn-accent btn-sm uppercase'
+            to='/products'>
+            reset
+        </Link>
+    </Form >
 }
 
 export default Filters
