48c48c9aaa4305d33bdef16a539acd6c925b6496
inga-divra <ingady@gmail.com>
2024-08-26 12:55:51 +0300

    Set up the CartTotals component using useSelector to retrieve cart state values and formatPrice for price formatting

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -1,8 +1,32 @@
+import { useSelector } from "react-redux"
+import { formatPrice } from "../utils"
 
 const CartTotals = () => {
-    return (
-        <h1 className='text-3xl'>CartTotals</h1>
-    )
+    const { cartTotal, shipping, tax, orderTotal } = useSelector((state) => state.cartState)
+    return <div className='card bg-base-200'>
+        <div className='card-body'>
+            {/* SUBTOTAL */}
+            <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+                <span className='capitalize'>subtotal</span>
+                <span className='font-medium'>{formatPrice(cartTotal)}</span>
+            </p>
+            {/* SHIPPING */}
+            <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+                <span className='capitalize'>shipping</span>
+                <span className='font-medium'>{formatPrice(shipping)}</span>
+            </p>
+            {/* TAX */}
+            <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+                <span className='capitalize'>Tax</span>
+                <span className='font-medium'>{formatPrice(tax)}</span>
+            </p>
+            {/* ORDER TOTAL */}
+            <p className='flex justify-between text-sm mt-4  pb-2'>
+                <span className='capitalize'>order total</span>
+                <span className='font-medium'>{formatPrice(orderTotal)}</span>
+            </p>
+        </div>
+    </div>
 }
 
 export default CartTotals
