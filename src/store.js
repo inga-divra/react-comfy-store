19ec3209f73210091e348e46324ee16e22430584
inga-divra <ingady@gmail.com>
2024-08-26 14:42:50 +0300

    Set up the userSlice
    Set up the userSlice with initial state containing a default user and theme, implemented basic reducers for loginUser, logoutUser, and toggleTheme, and integrated userReducer into the Redux store configuration.

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import cartReducer from './features/cart/cartSlice';
+import userReducer from './features/user/userSlice';
+import userSlice from './features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     cartState: cartReducer,
+    userState: userReducer,
   },
 });
