568075c3cd27dddc5c53e7f26d2f776c27d82ccd
inga-divra <ingady@gmail.com>
2024-08-26 13:52:19 +0300

    Set up the CartItem component to display product details, including title, price, company, color, and amount, with options to edit quantity and remove the item, using formatPrice and generateAmountOptions utilities

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,8 +1,56 @@
+import { formatPrice, generateAmountOptions } from '../utils';
+import { removeItem, editItem } from '../features/cart/cartSlice';
+import { useDispatch } from 'react-redux';
 
-const CartItem = () => {
-    return (
-        <h1 className='text-3xl'>CartItem</h1>
-    )
+const CartItem = ({ cartItem }) => {
+    const { cartID, title, price, image, amount, company, productColor } = cartItem
+
+
+    return <article
+        className='mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b 
+        border-base-30 pb-6 last:border-b-0'
+        key={cartID}>
+        {/* IMAGE */}
+        <img
+            className='h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover'
+            src={image}
+            alt={title} />
+        {/* INFO */}
+        <div className='sm:ml-16 sm:w-48'>
+            {/* TITLE */}
+            <h3 className='capitalize font-medium'>
+                {title}
+            </h3>
+            {/* COMPANY */}
+            <h4 className='mt-2 capitalize text-sm text-neutral-content'>{company}</h4>
+            {/* COLOR */}
+            <p className='mt-4 text-sm capitalize flex items-center gap-x-2'>
+                color:
+                <span className='badge badge-sm' style={{ backgroundColor: productColor }}></span>
+            </p>
+        </div>
+        <div className='sm:ml-24'>
+            {/* AMOUNT */}
+            <div className='form-control max-w-xs'>
+                <label htmlFor='amount' className='label p-0'>
+                    <span className='label-text capitalize'>
+                        amount
+                    </span>
+                </label>
+                <select className='mt-2 select select-base select-bordered select-xs' name='amount' id='amount'>
+                    {generateAmountOptions(amount + 5)}
+                </select>
+            </div>
+            {/* REMOVE */}
+            <button className='mt-2 link link-primary link-hover text-sm capitalize'>
+                remove
+            </button>
+        </div>
+        {/* PRICE */}
+        <p className='font-medium sm:ml-auto'>
+            {formatPrice(price)}
+        </p>
+    </article>
 }
 
 export default CartItem
