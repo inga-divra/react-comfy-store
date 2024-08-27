32da8a9e22d7004c27f83145b6a79b58651d2d40
inga-divra <ingady@gmail.com>
2024-08-27 19:26:55 +0300

    Set up logout functionality and user access across the Header, NavLinks, and Cart components.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,44 @@
-import { Link } from "react-router-dom"
-
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { logoutUser } from '../features/user/userSlice';
+import { clearCart } from '../features/cart/cartSlice';
 const Header = () => {
-    return (
-        <header className="bg-neutral py-2 text-neutral-content">
-            <div className="align-element flex justify-center sm:justify-end">
-                {/* USER */}
-                {/* LINKS */}
-                <div className="flex gap-x-6 justify-center items-center">
-                    <Link to="/login" className="link link-hover text-xs sm:text-sm" >
-                        Sign in / Guest
-                    </Link>
-                    <Link to="/register" className="link link-hover text-xs sm:text-sm" >
-                        Create Account
-                    </Link>
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.userState.user);
 
-                </div>
+    const handleLogout = () => {
+        navigate('/');
+        dispatch(clearCart());
+        dispatch(logoutUser());
+    };
+    return (
+        <header className=' bg-neutral py-2 text-neutral-content '>
+            <div className='align-element flex justify-center sm:justify-end '>
+                {user ? (
+                    <div className='flex gap-x-2 sm:gap-x-8 items-center'>
+                        <p className='text-xs sm:text-sm'>
+                            Hello, {user.username.charAt(0).toUpperCase() + user.username.slice(1)}
+                        </p>
+                        <button
+                            className='btn btn-xs btn-outline btn-primary uppercase'
+                            onClick={handleLogout}
+                        >
+                            logout
+                        </button>
+                    </div>
+                ) : (
+                    <div className='flex gap-x-6 justify-center items-center'>
+                        <Link to='/login' className='link link-hover text-xs sm:text-sm'>
+                            Sign in / Guest
+                        </Link>
+                        <Link to='/register' className='link link-hover text-xs sm:text-sm'>
+                            Create an Account
+                        </Link>
+                    </div>
+                )}
             </div>
         </header>
-
-    )
-}
-
-export default Header
+    );
+};
+export default Header;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,8 +3,7 @@ import { CartItemsList, SectionTitle, CartTotals } from '../components';
 import { Link } from 'react-router-dom';
 
 const Cart = () => {
-    // temp
-    const user = null;
+    const user = useSelector((state) => state.userState.user)
     const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
     if (numItemsInCart === 0) {
         return <SectionTitle text='Your cart is empty' />
@@ -19,7 +18,7 @@ const Cart = () => {
                 <div className='lg:col-span-4 lg:pl-4'>
                     <CartTotals />
                     {user ? (
-                        <Link to='/checkout' className='btn btn-primary btn-block mt-8'>
+                        <Link to='/checkout' className='btn btn-primary btn-block mt-8 uppercase'>
                             Proceed to checkout
                         </Link>
                     ) : (
