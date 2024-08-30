c5fa82bbfb583dee496c0361b6a15bd1a6248da7
inga-divra <ingady@gmail.com>
2024-08-30 16:46:46 +0300

    Implemented demo user login in Login.jsx
    - Removed default values from input fields.
    - Added loginAsGuestUser function to handle guest user login.
    - Integrated API call for guest login with customFetch.
    - Implemented loginUser action dispatch and success/error handling with toast notifications.
    - Added navigation to home page upon successful guest login.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { Form, Link, redirect, useNavigate } from 'react-router-dom';
 import { customFetch } from '../utils';
 import { toast } from 'react-toastify';
 import { loginUser } from '../features/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 
 export const action = (store) => async ({ request }) => {
@@ -22,6 +23,25 @@ export const action = (store) => async ({ request }) => {
 }
 
 const Login = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const loginAsGuestUser = async () => {
+        try {
+            const response = await customFetch.post('/auth/local', {
+                identifier: 'test@test.com',
+                password: 'secret'
+            })
+            dispatch(loginUser(response.data))
+            toast.success('welcome guest user')
+            navigate('/')
+        } catch (error) {
+            console.log(error);
+            toast.error('quest user error,please try again')
+
+        }
+    }
+
     return (
         <section className="h-screen grid place-items-center">
             <Form method="POST"
@@ -34,18 +54,18 @@ const Login = () => {
                     type="email"
                     label="email"
                     name="identifier"
-                    defaultValue="test@test.com" />
+                />
                 <FormInput
                     type="password"
                     label="password"
-                    name="password"
-                    defaultValue="secret" />
+                    name="password" />
                 <div className="mt-4">
                     <SubmitBtn text="login" />
                 </div>
                 <button
                     className="btn btn-secondary btn-block uppercase"
-                    type="button">
+                    type="button"
+                    onClick={loginAsGuestUser}>
                     guest user
                 </button>
                 <p className="text-center">
