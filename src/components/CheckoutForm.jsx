f6887cb466913530413026279d293a850a4a8560
inga-divra <ingady@gmail.com>
2024-08-30 19:06:02 +0300

    Implemented auth error handling in CheckoutForm.jsx
    - Added check for response status to handle unauthorized (401) errors.
    - Redirect user to login page if authentication error occurs during order submission.
    - Enhanced error handling with appropriate toast notifications.

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -37,6 +37,7 @@ export const action = (store) => async ({ request }) => {
         console.log(error);
         const errorMessage = error?.response?.data?.error?.message || 'there was an error placing your order'
         toast.error(errorMessage)
+        if (error.response.status === 401) return redirect('/login')
         return null
     }
 }
