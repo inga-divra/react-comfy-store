a8de02830f377201443c4ab6083ebbe11d0e579a
inga-divra <ingady@gmail.com>
2024-08-30 16:12:59 +0300

    Implemented user login action in Login.jsx
    - Added form data handling and user login logic using customFetch.
    - Implemented loginUser action dispatch with response data using Redux.
    - Added success and error handling with toast notifications.
    - Implemented redirection to home page upon successful login.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -22,7 +22,7 @@ const Cart = () => {
                             Proceed to checkout
                         </Link>
                     ) : (
-                        <Link to='/login' className='btn btn-primary btn-block mt-8'>
+                        <Link to='/login' className='btn btn-primary btn-block mt-8 uppercase'>
                             please login
                         </Link>
                     )}
