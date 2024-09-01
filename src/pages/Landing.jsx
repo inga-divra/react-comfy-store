1321ed873f507d279d1a88aac122a0e2feb3aed8
inga-divra <ingady@gmail.com>
2024-09-01 15:25:53 +0300

    Setup React Query to cache data in Landing and prevent redundant server requests
    - Configured query caching to avoid unnecessary server requests on page re-renders.
    - Improved performance by serving cached data for previously visited pages.

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,8 +3,13 @@ import { customFetch } from "../utils"
 
 const url = '/products?featured=true'
 
+const featuredProducts = {
+    queryKey: ['featuredProducts'],
+    queryFn: () => customFetch(url)
+}
+
 export const loader = (queryClient) => async () => {
-    const response = await customFetch(url)
+    const response = await queryClient.ensureQueryData(featuredProducts)
     const products = response.data.data
     return { products }
 }
